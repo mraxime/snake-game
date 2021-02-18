b32dbbc5a32e081f189c28f87e0c75d7be61fa69
Maxime50 <maxime.romero50@gmail.com>
2021-02-17 23:03:03 -0500

    Updates

---
+++ precache-manifest.b9568fd647ca33efd4017f2ff5b8e6ba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "890cd06277ef699a9e784f68cb1a543a",
+    "url": "/snake-game/index.html"
+  },
+  {
+    "revision": "6b3db50128ad45d43e7a",
+    "url": "/snake-game/static/css/main.55a51eb5.chunk.css"
+  },
+  {
+    "revision": "a8c8572f8755cc28c8f9",
+    "url": "/snake-game/static/js/2.b487702a.chunk.js"
+  },
+  {
+    "revision": "5843ec24999d9bc4d5892cbe88e32df5",
+    "url": "/snake-game/static/js/2.b487702a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b3db50128ad45d43e7a",
+    "url": "/snake-game/static/js/main.fced1e0d.chunk.js"
+  },
+  {
+    "revision": "18005e22df855f84fc12",
+    "url": "/snake-game/static/js/runtime-main.f9e1bf59.js"
+  }
+]);
